0c16ebdc0f94c25f7dff512d401864366299b991
Grant Sander <gksander93@gmail.com>
2024-06-15 23:10:25 -0500

    Animate some shit

--- src/components/homepage/ExpandableBackground.tsx
+++ src/components/homepage/ExpandableBackground.tsx
@@ -1,5 +1,7 @@
+import { FaCheck } from "@components/FaCheck";
 import { OptimizedImage } from "@components/OptimizedImage";
 import { clsx } from "clsx";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import type { OptimizedImageDetails } from "src/utils/getOptimizedImageSrc";
 
@@ -10,6 +12,7 @@ type Props = {
     description: string;
     time: string;
     logo?: OptimizedImageDetails;
+    accomplishments?: string[];
   }[];
 };
 
@@ -17,56 +20,76 @@ export function ExpandableBackground({ items }: Props) {
   const [expanded, setExpanded] = useState(false);
 
   const visibleItems = expanded ? items : items.slice(0, MAX_ITEMS_SHOWN);
+  const restItems = items.slice(MAX_ITEMS_SHOWN);
 
   return (
     <div className="flex flex-col gap-10">
-      {visibleItems.map((item, index) => (
-        <ExperienceItemCard
-          key={item.time}
-          item={item}
-          isPrimary={index === 0}
-        />
-      ))}
-      {!expanded && (
-        <div>
-          <button
-            onClick={() => setExpanded(true)}
-            className="rounded p-2 -ml-2 text-sm font-bold"
-          >
-            And more...
-          </button>
-        </div>
-      )}
+      <AnimatePresence initial={false} mode="popLayout">
+        {visibleItems.map((item, idx) => (
+          <ExperienceItemCard
+            key={item.time}
+            item={item}
+            overflowIdx={Math.max(idx - MAX_ITEMS_SHOWN + 1, 0)}
+          />
+        ))}
+
+        {!expanded ? (
+          <motion.div initial={{ opacity: 1 }} exit={{ opacity: 0 }}>
+            <button
+              onClick={() => setExpanded(true)}
+              className="rounded p-2 -ml-2 text-sm font-bold"
+            >
+              And more...
+            </button>
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
     </div>
   );
 }
 
 function ExperienceItemCard({
-  isPrimary,
   item,
+  overflowIdx = 0,
 }: {
   item: Props["items"][number];
-  isPrimary?: boolean;
+  overflowIdx?: number;
 }) {
   return (
-    <div className="flex gap-x-4 items-end">
-      <div
-        className={clsx(
-          "flex flex-col gap-y-3 flex-1",
-          isPrimary ? "bg-opacity-90" : "bg-opacity-25",
-        )}
-      >
+    <motion.div
+      layout
+      className="flex gap-x-4 items-end"
+      initial={{ opacity: 0, y: 5 }}
+      animate={{
+        opacity: 1,
+        y: 0,
+        transition: { delay: overflowIdx * 0.15, duration: 0.3 },
+      }}
+    >
+      <div className={clsx("flex flex-col gap-y-3 flex-1")}>
         <div>
-          <div className="text-lg font-bold leading-snug">{item.company}</div>
-          <div className="text-black-light text-sm flex gap-x-2 items-center">
-            {item.title} <span>&middot;</span>
-            <span
-              className="text-xs"
-              dangerouslySetInnerHTML={{ __html: item.time }}
-            />
+          <div
+            dangerouslySetInnerHTML={{ __html: item.time }}
+            className="text-xs text-black-light font-medium"
+          />
+          <div className="text-lg font-bold">
+            {item.title} @ {item.company}
           </div>
         </div>
         <div className="text-subtle-copy text-sm">{item.description}</div>
+        {item.accomplishments && (
+          <ul className="text-sm text-subtle-copy flex flex-col gap-y-0.5">
+            {item.accomplishments.map((accomplishment) => (
+              <li
+                key={accomplishment}
+                className="text-subtle-copy flex gap-x-2 items-center"
+              >
+                <FaCheck className="w-2" />
+                {accomplishment}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
 
       {item.logo && (
@@ -76,7 +99,7 @@ function ExperienceItemCard({
           className="block w-16 h-16 object-contain aspect-square flex-shrink-0 opacity-30 grayscale"
         />
       )}
-    </div>
+    </motion.div>
   );
 }
 
