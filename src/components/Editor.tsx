1eb55cba1297c8eebaacec2228764566cc1c365b
Grant Sander <gksander93@gmail.com>
2024-06-22 07:07:27 -0500

    Loading overlay

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import {
   Sandpack,
   type SandpackPredefinedTemplate,
@@ -11,8 +12,14 @@ type Props = {
 };
 
 export function Editor({ template = "react-ts", files, activeFile }: Props) {
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
-    <div className="-mx-breakout-4 md:-mx-breakout-8 sp-editor">
+    <div className="-mx-breakout-4 md:-mx-breakout-8 sp-editor relative isolate">
       <Sandpack
         template={template}
         theme={atomDark}
@@ -26,6 +33,10 @@ export function Editor({ template = "react-ts", files, activeFile }: Props) {
           activeFile,
         }}
       />
+
+      {!isMounted && (
+        <div className="absolute inset-0 bg-background/95 backdrop-blur-lg z-10" />
+      )}
     </div>
   );
 }
